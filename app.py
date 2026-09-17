d7628ab6ca2375ba492dbccdf52c94a7248acff5
spamyouracc-spec <spamyouracc@gmail.com>
2026-09-17 10:40:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-with open("report.txt", "w")asf:
-f.write("ApplicationReport\n")
-f.write("TotalUsers:120\n")
-f.write("ActiveSessions:45\n")
-print("Reportgenerated.")
+with open("report.txt", "w") as f:
+    f.write("ApplicationReport\n")
+    f.write("TotalUsers:120\n")
+    f.write("ActiveSessions:45\n")
+
+print("Report generated.")
